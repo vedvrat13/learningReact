c2907fd608e2c14dca338b81aa081e1a699df424
Vedvrat Sharma <vedvrat.mca@gmail.com>
2016-05-31 15:43:25 +0530

    [ BLOGROLL ] - Completed Delete

--- BlogRoll/src/actions/index.js
+++ BlogRoll/src/actions/index.js
@@ -2,23 +2,28 @@ import axios from 'axios';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POSTS = 'CREATE_POSTS';
+export const FETCH_POST = 'FETCH_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api/';
-const API_KEY = '?key=123';
+const API_KEY = '?key=123dd';
 
 export function fetchPosts() {
     const response = axios.get(`${ROOT_URL}posts${API_KEY}`);
-    return {
-        type: FETCH_POSTS,
-        payload: response
-    };
+    return {type: FETCH_POSTS, payload: response};
 }
 
 export function createPost(props) {
     const request = axios.post(`${ROOT_URL}posts${API_KEY}`, props);
+    return {type: CREATE_POSTS, payload: request};
+}
 
-    return {
-        type: CREATE_POSTS,
-        payload: request
-    };
+export function fetchPost(id) {
+    const request = axios.get(`${ROOT_URL}posts/${id}${API_KEY}`);
+    return {type: FETCH_POST, payload: request}
+}
+
+export function deletePost(id) {
+    const request = axios.delete(`${ROOT_URL}posts/${id}${API_KEY}`);
+    return {type: DELETE_POST, payload: request};
 }
--- BlogRoll/src/components/posts_show.js
+++ BlogRoll/src/components/posts_show.js
@@ -1,11 +1,39 @@
-import React, {Component} from 'react';
+import React, {Component,PropTypes } from 'react';
+import {connect} from 'react-redux';
+import {fetchPost, deletePost} from '../actions/index';
+import {Link} from 'react-router';
 
 class PostsShow extends Component {
+    static contextTypes = {
+        router: PropTypes.object
+    };
+    componentWillMount() {
+        this.props.fetchPost(this.props.params.id);
+    }
+    onDeleteClick(){
+        this.props.deletePost(this.props.params.id).then(()=>{
+            this.context.router.push('/');
+        });
+    }
     render() {
+        const {post} = this.props;
+        if (!post) {
+            return <div>Loading..</div>
+        }
         return (
-            <div>Posts show {this.props.params.id}</div>
+            <div>
+                <Link to="/">Back to Main Page</Link>
+                <button className="btn btn-danger pull-xs-right" onClick={() => this.onDeleteClick()}>Delete Post</button>
+                <h3>{post.title}</h3>
+                <h5>Category: {post.categories}</h5>
+                <p>{post.content}</p>
+            </div>
         );
     }
 }
 
-export default PostsShow;
+function mapStateToProps(state) {
+    return {post: state.posts.post}
+}
+
+export default connect(mapStateToProps, {fetchPost, deletePost})(PostsShow);
